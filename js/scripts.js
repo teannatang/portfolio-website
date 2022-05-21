7eef498c65a50445f40d41fd6de3dbb89cf3e940
teannatang <tangteanna@gmail.com>
2022-05-20 23:43:12 -0400

    upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+console.log("Hello World!")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
